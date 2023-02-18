4ab12f733b3a6a5697e5254a8df65888decce519
Ali Reza <webdeve1083@gmail.com>
2023-02-18 12:58:25 +0800

    All files are ready...

---
+++ pages/docs/[...slug].tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+export default function Docs({}: Props) {
+  return (
+    <div>
+      <nav></nav> {/* Navbar here */}
+      <div></div> {/* MDXContent here */}
+    </div>
+  );
+}
